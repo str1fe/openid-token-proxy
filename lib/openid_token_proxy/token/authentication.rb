2007ce1277068594d1719ea4e2308060a86f13e6
Pål Andre Sundt <pal@rubynor.com>
2015-08-21 11:51:02 +0200

    Log errors to Appsignal

--- lib/openid_token_proxy/token/authentication.rb
+++ lib/openid_token_proxy/token/authentication.rb
@@ -12,7 +12,7 @@ module OpenIDTokenProxy
       module ClassMethods
         def require_valid_token(*args)
           before_action :require_valid_token, *args
-          after_action :expose_token_expiry_time
+          after_action :expose_token_expiry_time, *args
         end
       end
 
@@ -23,6 +23,7 @@ module OpenIDTokenProxy
 
       def require_authorization(exception)
         set_authentication_url!
+        Appsignal.add_exception(exception)
         render json: exception.to_json, status: :unauthorized
       end
 
